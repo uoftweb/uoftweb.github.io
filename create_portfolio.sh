320e2862a8cc497b7f7c437e1640279e13f52291
JLambertazzo <jbertazzolambert@gmail.com>
2022-09-10 13:12:08 -0400

    feat: initial shell and template

---
+++ create_portfolio.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+echo "Hi there! What's your name?"
+read NAME
+
+echo "And what year are you in, $NAME?"
+read YEAR
+
+echo "Please describe yourself in one sentence!"
+read SUMMARY
+
