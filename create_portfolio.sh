b80d040bb82bf6532e81249ea676a056edc3180c
JLambertazzo <jbertazzolambert@gmail.com>
2022-09-11 17:30:32 -0400

    feat: finished create portfolio script

--- create_portfolio.sh
+++ create_portfolio.sh
@@ -9,3 +9,25 @@ read YEAR
 echo "Please describe yourself in one sentence!"
 read SUMMARY
 
+echo Building your site...
+DIR="../autosite/$NAME-$YEAR"
+mkdir $DIR
+cp template.html "$DIR/index.html"
+cp template.css "$DIR/style.css"
+
+# replacing stuff
+sed -i '' "s/template\.css/style\.css/g" "$DIR/index.html"
+sed -i '' "s/VAR_NAME/$NAME/g" "$DIR/index.html"
+sed -i '' "s/VAR_SUMMARY/$SUMMARY/g" "$DIR/index.html"
+
+# create git project
+echo Initializing git
+cd $DIR
+git init -b main
+git stage *
+git commit -m "🥳 initial commit"
+
+# create gh project
+gh repo create "uoftweb/$NAME" --source=. --public --push
+echo Done creating project, turn on pages to go public!
+open "https://github.com/uoftweb/$NAME/settings/pages"
